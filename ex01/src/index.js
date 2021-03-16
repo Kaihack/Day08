7dd0564b3f4c13139b08d4a3b2518926589ec1e0
Kaihack <prostor.za.rad@gmail.com>
2021-03-16 09:00:07 +0100

    Added index.js - Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+// Writte code bellow this line
+function reusableFunction() {
+    console.log("Hi Padawan!");
+}
+reusableFunction();
+
+// Writte code above this line
+
+module.exports = reusableFunction;
