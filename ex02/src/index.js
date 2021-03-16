3815a724a0963c8b7631afb97af885aa4f95dfc6
Kaihack <prostor.za.rad@gmail.com>
2021-03-16 09:01:30 +0100

    Added index.js - Functions with Arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+// Writte code bellow this line
+function functionPrintInfo( name, age ) {
+    console.log( name, age );
+}
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+
+// Writte code above this line
+
+module.exports = functionPrintInfo;
