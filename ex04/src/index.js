c124b524f3d6fa862c60349820cb6b7775d74800
Kaihack <prostor.za.rad@gmail.com>
2021-03-16 09:05:25 +0100

    Added index.js - Local Scope

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+// Only change code bellow this line
+
+function localScope() {
+var myVariable = "I am here";
+console.log(myVariable);
+}
+
+localScope();
+// Only change code above this line
+
+
+// myVariable is not defined outside of localScope
+
+if (typeof myVariable != "undefined" ) {
+console.log('outside localScope', myVariable)
+} else {
+    console.log('outside localScope, UNDEFINED!!!')
+}
